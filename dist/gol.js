53148f6624b7b942a2753b57efd4d1d236984ecc
Raoul Harel <raoulhaa@gmail.com>
2015-08-26 14:05:18 +0200

    updated build

--- dist/gol.js
+++ dist/gol.js
@@ -6,209 +6,220 @@
  */
 
 
-function World(width, height) {
+(function() {
 
-  this._width = width;
-  this._height = height;
+  /**
+   * @author Raoul Harel
+   * @license The MIT license (LICENSE.txt)
+   * @copyright 2015 Raoul Harel
+   * @url https://github.com/rharel/js-gol
+   */
 
-  this._cells = {};
-  this._neighbours = {};
-  this._dirty = {};
 
-  this._population = 0;
+  function World(width, height) {
 
-  this._spawned = [];
-  this._died = [];
-}
+    this._width = width;
+    this._height = height;
 
-World.prototype = {
+    this._cells = {};
+    this._neighbours = {};
+    this._dirty = {};
 
-  constructor: World,
-
-  _isOutside: function(x, y) {
-
-    return x < 0 || x >= this._width ||
-           y < 0 || y >= this._height;
-  },
-
-  _setDirty: function(x, y) {
-
-    if (!this._dirty.hasOwnProperty(x)) { this._dirty[x] = {}; }
-
-    this._dirty[x][y] = true;
-  },
-
-  _addNeighbour: function(x, y) {
-
-    x = x === this._width ? 0 : x === -1 ? this._width - 1 : x;
-    y = y === this._height ? 0 : y === -1 ? this._height - 1 : y;
-
-    if (!this._neighbours.hasOwnProperty(x)) { this._neighbours[x] = {}; }
-    if (!this._neighbours[x].hasOwnProperty(y)) { this._neighbours[x][y] = 0; }
-
-    this._neighbours[x][y] += 1;
-
-    this._setDirty(x, y);
-  },
-
-  _removeNeighbour: function(x, y) {
-
-    x = x === this._width ? 0 : x === -1 ? this._width - 1 : x;
-    y = y === this._height ? 0 : y === -1 ? this._height - 1 : y;
-
-    this._neighbours[x][y] -= 1;
-
-    if (this._neighbours[x][y] === 0) {
-
-      delete this._neighbours[x][y];
-    }
-
-    this._setDirty(x, y);
-  },
-
-  get: function(x, y) {
-
-    if (this._cells.hasOwnProperty(x)) {
-
-      var column = this._cells[x];
-      if (column.hasOwnProperty(y)) { return true; }
-    }
-
-    return false;
-  },
-
-  spawn: function(x, y) {
-
-    if (this._isOutside(x, y) || this.get(x, y)) { return false; }
-
-    if (!this._cells.hasOwnProperty(x)) { this._cells[x] = {}; }
-    this._cells[x][y] = true;
-
-    ++ this._population;
-
-    this._addNeighbour(x - 1, y - 1);
-    this._addNeighbour(x - 1, y);
-    this._addNeighbour(x - 1, y + 1);
-    this._addNeighbour(x, y - 1);
-    this._addNeighbour(x, y + 1);
-    this._addNeighbour(x + 1, y - 1);
-    this._addNeighbour(x + 1, y);
-    this._addNeighbour(x + 1, y + 1);
-
-    this._setDirty(x, y);
-
-    return true;
-  },
-
-  kill: function(x, y) {
-
-    if (this._isOutside(x, y) || !this.get(x, y)) { return false; }
-
-    delete this._cells[x][y];
-
-    -- this._population;
-
-    this._removeNeighbour(x - 1, y - 1);
-    this._removeNeighbour(x - 1, y);
-    this._removeNeighbour(x - 1, y + 1);
-    this._removeNeighbour(x, y - 1);
-    this._removeNeighbour(x, y + 1);
-    this._removeNeighbour(x + 1, y - 1);
-    this._removeNeighbour(x + 1, y);
-    this._removeNeighbour(x + 1, y + 1);
-
-    this._setDirty(x, y);
-
-    return true;
-  },
-
-  _step_single: function(x, y) {
-
-    var currentState = this.get(x, y);
-    var nNeighbours = +currentState;
-
-    if (this._neighbours.hasOwnProperty(x) &&
-        this._neighbours[x].hasOwnProperty(y)) {
-
-      nNeighbours += this._neighbours[x][y];
-    }
-
-    if (nNeighbours === 3) { return true; }
-    if (nNeighbours === 4) { return currentState; }
-    else { return false; }
-  },
-
-  step: function() {
+    this._population = 0;
 
     this._spawned = [];
     this._died = [];
+  }
 
-    for (var x in this._dirty) {
+  World.prototype = {
 
-      if (!this._dirty.hasOwnProperty(x)) { continue; }
+    constructor: World,
 
-      var column = this._dirty[x];
-      for (var y in column) {
+    _isOutside: function(x, y) {
 
-        if (!column.hasOwnProperty(y)) { continue; }
+      return x < 0 || x >= this._width ||
+        y < 0 || y >= this._height;
+    },
 
-        var result = this._step_single(x, y);
+    _setDirty: function(x, y) {
 
-        if (result && !this.get(x, y)) { this._spawned.push({x: +x, y: +y}); }
-        else if (!result && this.get(x, y)) { this._died.push({x: +x, y: +y}); }
+      if (!this._dirty.hasOwnProperty(x)) { this._dirty[x] = {}; }
+
+      this._dirty[x][y] = true;
+    },
+
+    _addNeighbour: function(x, y) {
+
+      x = x === this._width ? 0 : x === -1 ? this._width - 1 : x;
+      y = y === this._height ? 0 : y === -1 ? this._height - 1 : y;
+
+      if (!this._neighbours.hasOwnProperty(x)) { this._neighbours[x] = {}; }
+      if (!this._neighbours[x].hasOwnProperty(y)) { this._neighbours[x][y] = 0; }
+
+      this._neighbours[x][y] += 1;
+
+      this._setDirty(x, y);
+    },
+
+    _removeNeighbour: function(x, y) {
+
+      x = x === this._width ? 0 : x === -1 ? this._width - 1 : x;
+      y = y === this._height ? 0 : y === -1 ? this._height - 1 : y;
+
+      this._neighbours[x][y] -= 1;
+
+      if (this._neighbours[x][y] === 0) {
+
+        delete this._neighbours[x][y];
       }
-    }
 
-    this._dirty = {};
+      this._setDirty(x, y);
+    },
 
-    this._spawned.forEach(
-      (function(p) {
+    get: function(x, y) {
 
-        this.spawn(p.x, p.y);
+      if (this._cells.hasOwnProperty(x)) {
 
-      }).bind(this)
-    );
-
-    this._died.forEach(
-      (function(p) {
-
-        this.kill(p.x, p.y);
-
-      }).bind(this)
-    );
-  },
-
-  traverse: function(callback) {
-
-    var abortRequested = false;
-
-    for (var x in this._cells) {
-
-      if (!this._cells.hasOwnProperty(x)) { continue; }
-
-      for (var y in this._cells[x]) {
-
-        if (this._cells[x].hasOwnProperty(y)) { abortRequested = callback(x, y); }
-
-        if (abortRequested) { return; }
+        var column = this._cells[x];
+        if (column.hasOwnProperty(y)) { return true; }
       }
-    }
-  },
 
-  get width() { return this._width; },
-  get height() { return this._height; },
+      return false;
+    },
 
-  get population() { return this._population; }
-};
+    spawn: function(x, y) {
 
+      if (this._isOutside(x, y) || this.get(x, y)) { return false; }
 
-if (typeof exports !== 'undefined') {
+      if (!this._cells.hasOwnProperty(x)) { this._cells[x] = {}; }
+      this._cells[x][y] = true;
 
-  exports.World = World;
-}
-else if (typeof window !== 'undefined') {
+      ++ this._population;
 
-  window.Gol = {
-    World: World
+      this._addNeighbour(x - 1, y - 1);
+      this._addNeighbour(x - 1, y);
+      this._addNeighbour(x - 1, y + 1);
+      this._addNeighbour(x, y - 1);
+      this._addNeighbour(x, y + 1);
+      this._addNeighbour(x + 1, y - 1);
+      this._addNeighbour(x + 1, y);
+      this._addNeighbour(x + 1, y + 1);
+
+      this._setDirty(x, y);
+
+      return true;
+    },
+
+    kill: function(x, y) {
+
+      if (this._isOutside(x, y) || !this.get(x, y)) { return false; }
+
+      delete this._cells[x][y];
+
+      -- this._population;
+
+      this._removeNeighbour(x - 1, y - 1);
+      this._removeNeighbour(x - 1, y);
+      this._removeNeighbour(x - 1, y + 1);
+      this._removeNeighbour(x, y - 1);
+      this._removeNeighbour(x, y + 1);
+      this._removeNeighbour(x + 1, y - 1);
+      this._removeNeighbour(x + 1, y);
+      this._removeNeighbour(x + 1, y + 1);
+
+      this._setDirty(x, y);
+
+      return true;
+    },
+
+    _step_single: function(x, y) {
+
+      var currentState = this.get(x, y);
+      var nNeighbours = +currentState;
+
+      if (this._neighbours.hasOwnProperty(x) &&
+        this._neighbours[x].hasOwnProperty(y)) {
+
+        nNeighbours += this._neighbours[x][y];
+      }
+
+      if (nNeighbours === 3) { return true; }
+      if (nNeighbours === 4) { return currentState; }
+      else { return false; }
+    },
+
+    step: function() {
+
+      this._spawned = [];
+      this._died = [];
+
+      for (var x in this._dirty) {
+
+        if (!this._dirty.hasOwnProperty(x)) { continue; }
+
+        var column = this._dirty[x];
+        for (var y in column) {
+
+          if (!column.hasOwnProperty(y)) { continue; }
+
+          var result = this._step_single(x, y);
+
+          if (result && !this.get(x, y)) { this._spawned.push({x: +x, y: +y}); }
+          else if (!result && this.get(x, y)) { this._died.push({x: +x, y: +y}); }
+        }
+      }
+
+      this._dirty = {};
+
+      this._spawned.forEach(
+        (function(p) {
+
+          this.spawn(p.x, p.y);
+
+        }).bind(this)
+      );
+
+      this._died.forEach(
+        (function(p) {
+
+          this.kill(p.x, p.y);
+
+        }).bind(this)
+      );
+    },
+
+    traverse: function(callback) {
+
+      var abortRequested = false;
+
+      for (var x in this._cells) {
+
+        if (!this._cells.hasOwnProperty(x)) { continue; }
+
+        for (var y in this._cells[x]) {
+
+          if (this._cells[x].hasOwnProperty(y)) { abortRequested = callback(x, y); }
+
+          if (abortRequested) { return; }
+        }
+      }
+    },
+
+    get width() { return this._width; },
+    get height() { return this._height; },
+
+    get population() { return this._population; }
   };
-}
+
+
+  if (typeof exports !== 'undefined') {
+
+    exports.World = World;
+  }
+  else if (typeof window !== 'undefined') {
+
+    window.Gol = {
+      World: World
+    };
+  }
+})();
